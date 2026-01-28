1d5a88d5ec6fb56a8e8539e0e63c02e70c54b5da
GodblessOsei <gmo6996@nyu.edu>
2026-01-28 22:51:34 +0400

    Initial Commit

---
+++ StreakManager/StreakManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakManagerApp.swift
+//  StreakManager
+//
+//  Created by Godbless Mensah Osei  on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct StreakManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
